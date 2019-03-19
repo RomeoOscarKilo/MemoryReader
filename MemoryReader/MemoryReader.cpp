b88c8126ca8eaf97e2cfc8d62fa696e09934d3c0
alex seddon <alexander.seddon@ymail.com>
2019-03-19 20:12:03 +0000

    Added framework

--- MemoryReader/MemoryReader.cpp
+++ MemoryReader/MemoryReader.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 #include<Windows.h>
 #include<string>
-
+#include <sstream>
 
 
 
 using namespace std;
 void errorGen(string errorType, int exitType);
+string convertToHex(int number);
 int main()
 {
-
 	cout << "Please enter  program" << endl;
 	string programName;
 	getline(cin, programName);
 	HWND hwnd = FindWindowA(NULL, programName.c_str());
+
 	if (hwnd == NULL)
 	{
 		errorGen("Could not find program", -2); // -2 = Failed to find program name
@@ -26,22 +27,20 @@ int main()
 	{
 		errorGen("Could not find process ID", -3); // -3 = failed procces ID
 	}
-
-	 
-
 	return 0;
-
 }
 
+string convertToHex(int number)
+{	
+	stringstream ss;
+	ss << hex << number;
+	return "0x" + ss.str();
+}
 
 
 void errorGen(string errorType, int exitType)
 {
-	cout << errorType << endl;
+	cout << errorType << endl; // outputs text then outputs an end line
 	Sleep(3000);
-	exit(exitType);
-
-	//cout << "Test" << endl;
-	//Sleep(3000);
-	//exit(-1);
+	exit(exitType); // exits program with error type
 }
